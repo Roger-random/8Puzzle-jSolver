5c1cd024fd5ce926be064b1777313c1431ac6dc9
Roger Cheng <roger.random@outlook.com>
2016-03-12 22:40:59 +0000

    Recognition of solved state using Displaced Tiles and Manhattan Distance

--- slidingTiles.js
+++ slidingTiles.js
@@ -7,7 +7,10 @@ var tileBlank = 0;
 // 0 represents blank.
 var tilePosition = [1, 2, 3,
                     4, 5, 6, 
-                    7, 8, tileBlank]
+                    7, 8, tileBlank];
+
+var boardRows = 3;
+var boardColumns = 3;
 
 // How long to take to animate a tile
 var tileSlideTime = 25;
@@ -15,6 +18,9 @@ var tileSlideTime = 25;
 // How much space to put between tiles, in fraction of maximum tile space. 
 // (0.05 = 5% space)
 var tileSpace = 0.05;
+
+// Track the number of times the player moved a tile
+var playerMoves = 0;
 
 // Gets the length of a tile's side.
 // The item #tileBoard is told to lay itself out at 100% of available width.
@@ -25,10 +31,13 @@ var tileSpace = 0.05;
 var getTileDim = function() {
   var tileBoard = $("#tileBoard");
 
-  var minDim = Math.min(window.innerWidth, window.innerHeight, tileBoard.innerWidth());
+  var minDim = Math.min(
+    window.innerWidth, 
+    window.innerHeight-$("#otherUI").innerHeight(), 
+    tileBoard.innerWidth() );
 
   return (minDim / 3) * (1-tileSpace);
-}
+};
 
 // Given a tile number, return its index in the array.
 // Returns -1 if not found.
@@ -40,7 +49,7 @@ var indexOfTile = function(tileNum) {
   }
 
   return -1;
-}
+};
 
 // Given a tile number, starts an animation that moves the tile to its
 // corresponding location on screen. Call this after the tile has been
@@ -56,7 +65,7 @@ var updatePositionOfTile = function(tileNum) {
     "left": tileColumn * tileDim,
     "top": tileRow * tileDim
   }, tileSlideTime);
-}
+};
 
 // When the viewport is resized, update size of board accordingly.
 var resizeTiles = function() {
@@ -80,7 +89,7 @@ var resizeTiles = function() {
   }
 
   $("#tileBoard").css("height", (tileDim * (1/(1-tileSpace))) * 3);
-}
+};
 
 // Initial setup of game board. Take the HTML for ".box" under the
 // tileTemplateHost" and clone it 8 times for the game tile. 
@@ -97,7 +106,7 @@ var setupTiles = function() {
   }
 
   resizeTiles();
-}
+};
 
 // Checks to see if the two given tile indices can be legally swapped.
 // If so, return true.
@@ -116,7 +125,52 @@ var isValidSwap = function(indexClick, indexBlank) {
     return true; // Move up
   }
   return false;
-}
+};
+
+var calculateManhattanDistance = function(index) {
+  var tileAtIndex = tilePosition[index];
+  var desiredIndex = 0;
+  
+  if (tileAtIndex == tileBlank ) {
+    desiredIndex = tilePosition.length -1;
+  } else {
+    desiredIndex = tilePosition[index]-1;
+  }
+
+  var actualRow = Math.floor(index/boardColumns);
+  var desiredRow= Math.floor(desiredIndex/boardColumns);
+  
+  var actualColumn = index%boardColumns;
+  var desiredColumn = desiredIndex%boardColumns;
+  
+  return Math.abs(actualRow-desiredRow) + Math.abs(actualColumn-desiredColumn);
+};
+
+// Evaluates the board position and update the status text
+var updateStatusBar = function() {
+  var displacedTiles = 0;
+  var manhattanDistance = 0;
+  
+  for( var i = 0; i < tilePosition.length; i++) {
+    if (tilePosition[i] != 0 &&
+        tilePosition[i] != i+1) {
+      displacedTiles++;
+      
+      manhattanDistance += calculateManhattanDistance(i);
+    }
+  }
+  
+  if (displacedTiles == 0) {
+    $("#boardState").text("All tiles in correct position");
+    $("#progress").text(playerMoves + " moves were taken. Press SCRAMBLE PUZZLE to start again.");
+    $("#scrambleButton").css("background-color", "red");
+    $("#scrambleText").text("SCRAMBLE PUZZLE")
+    $("#scrambleButton").on("click", scramblePuzzle);
+  } else {
+    $("#boardState").text(displacedTiles + " tiles displaced by total of " + manhattanDistance + " spaces");
+    $("#progress").text(playerMoves + " moves so far.");
+  }
+};
 
 // Click event handler for .box delegated on the #tileBoard. $(this) is the 
 // tile that got clicked on. Retrieve its index in the tilePosition array and
@@ -132,8 +186,27 @@ var tileClicked = function(event) {
     tilePosition[indexBlank] = tileClick;
     tilePosition[indexClick] = tileBlank;
     updatePositionOfTile(tileClick);
-  };
-}
+  }
+  
+  playerMoves++;
+  updateStatusBar();
+};
+
+var scramblePuzzle = function(event) {
+  // Placeholder scramble until I write the real one.
+  tilePosition = [2, 6, 8, 
+                  4, tileBlank, 3, 
+                  1, 7, 5];
+  for( var i = 1; i < tilePosition.length; i++) {
+    updatePositionOfTile(i);
+  }
+  
+  playerMoves = 0;
+  updateStatusBar();
+  $("#scrambleButton").css("background-color", "gray");
+  $("#scrambleText").text("PUZZLE IN PROGRESS");
+  $("#scrambleButton").off("click", scramblePuzzle);
+};
 
 // Game board setup: generate tiles, size them correctly, and wait for the
 // user to click.
@@ -141,4 +214,6 @@ $(document).ready(function() {
   setupTiles();
   $(window).resize(resizeTiles);
   $("#tileBoard").on("click", ".box", tileClicked);
-})
+  $("#scrambleButton").on("click", scramblePuzzle);
+  
+});
